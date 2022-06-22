d0be1c935af0aeb177dd24b818f1d6c568076093
AliiDak <dakoumi31@gmail.com>
2022-06-22 20:14:57 +0100

    last

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter
-      basename={"https://ali-dakoumi.github.io/React-e-commerce-website/"}
+      basename={"https://ali-dakoumi.github.io/React-e-commerce-website"}
     >
       <App />
     </BrowserRouter>
